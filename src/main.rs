1a23238a6a279ddce6ae5190ab6a5c72be0438eb
aroesec <aroe3sec@gmail.com>
2022-08-22 20:18:47 -0600

    updated vector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let todos = store_todos();
+    get_todos(todos[1])
+    // let terminal_args: Vec<String> = env::args().collect();
+}
+
+fn store_todos() -> Vec {
+    let mut vector = Vec::new();
+    let added_todos: Vec<String> = env::args().collect();
+
+    for args in added_todos {
+        println!("{}", args);  
+        vector.push(args)
+    }
+    return vector
+}
+ 
+
+
+fn get_todos(todos:Vec<T>) {
+    //grab todos and display in order
+    println!("here are your todos")
 }
